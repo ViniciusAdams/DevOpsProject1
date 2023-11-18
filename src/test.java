4c5089b9d35d6d82d82f0ca65ff03e75a4de4244
vinia <viniusadams98@gmail.com>
2023-11-18 00:24:33 +0000

    adding jUnit test

---
+++ src/test.java
@@ -0,0 +1,10 @@
+// HelloWorldTest.java
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class test{
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Hello, DevOps in Java!", "Hello, DevOps in Java!");
+    }
+}
